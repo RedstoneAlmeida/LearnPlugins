6115c17abe4ac55e2fcd6a2bf62f2114e7e67863
RedstoneAlmeida <mineps3marcos@gmail.com>
2016-12-02 13:54:34 -0200

    Update BasicCommands.php

--- Commands/BasicCommands.php
+++ Commands/BasicCommands.php
@@ -16,6 +16,16 @@ class BasicCommands extends PluginBase {
     public function onCommand(CommandSender $sender, Command $command, $label, $args){
         switch($command->getName()){
               case "youcommand":
+                    // Sub-Commands /youcommand <sub[0]>
+                    if(isset($args[0]){
+                        $p1 = $this->getServer()->getPlayer($args[0]);
+                        // Sub-Commands /youcommand <sub[0]> <sub[1]>
+                        if(isset($args[1]){
+                            $p2 = $this->getServer()->getPlayer($args[1]);
+                            $sender->sendMessage("Player 1: ".$p1->getName());
+                            $sender->sendMessage("Player 2: ".$p2->getName());
+                        }
+                    }
                     break;
         }
     }
